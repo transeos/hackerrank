e4672ffd98675fb782f52f2c76582b0d3234a94a
transeos <hiranmoy.iitkgp@gmail.com>
2019-05-16 19:28:53 +0100

    adding python directory

---
+++ python/nested_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+  
+# -*- Python -*-
+#
+# hackerrank: https://www.hackerrank.com/challenges/nested-list/problem
+#
+# WARRANTY:
+# Use all material in this file at your own risk.
+#
+# Created by Hiranmoy Basak on 04/01/18.
+#
+
+print("nested-list")
